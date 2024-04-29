8ba997e21e6c4400010da56b390239811e60e37d
Utman <148863405+Rhyan2@users.noreply.github.com>
2024-04-29 21:28:52 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
 use std::collections::HashSet;
-
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{word}' word find anagrams among the following words: {possible_anagrams:?}"
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let word = word.to_lowercase();
+    let mut word_sorted: Vec<char> = word.chars().collect();
+    word_sorted.sort();
+    HashSet::from_iter(possible_anagrams.iter().filter(|&anagram| {
+        let anagram = anagram.to_lowercase();
+        let mut anagram_sorted: Vec<char> = anagram.chars().collect();
+        anagram_sorted.sort();
+        anagram_sorted == word_sorted 
+            && anagram != word       
+    }).cloned())
 }
